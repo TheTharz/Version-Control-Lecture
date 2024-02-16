1a21d21e091babd441e4b11aaa7afd6b956d6551
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-02-16 15:01:35 +0530

    testing

--- test.py
+++ test.py
@@ -3,9 +3,15 @@ import sys
 def default():
     print("Hello")
 
+def cat():
+    print("Meow")
+
 def main():
-    default()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'cat':
+            cat()
+    else:
+        default()
 
 if __name__ == '__main__':
-        main()
-
+    main()
